5efd2e7863163bb9b8fc7a46da9145bb47db5fe0
k.naito <ziputa0310@gmail.com>
2020-01-14 10:31:41 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # pystreamer
 
-* 以下のSERVERで接続先を指定しています。適宜変更して下さい。
+
+* 実行
+
+複数ストリーム取得
+```python
+
+$ python pystreamer.py --cameras rtsp://localhost:8554/live.sdp rtsp://localhost:8555/live.sdp
+
+```
+
+単一ストリーム取得
+```python
+
+$ python pystreamer.py --cameras rtsp://localhost:8554/live.sdp
+
+ or
+
+$ python pystreamer.py
+
+```
+
+
+* 単一ストリーム取得かつ--camerasオプションを利用しない場合、以下のSERVERで指定した接続先を利用しています。適宜変更して下さい。
 
 ```python
 
@@ -17,8 +39,18 @@ parser.add_argument("--debug", action='store_true')
 args = parser.parse_args()
 
 # RTSP Server
-SERVER = "localhost:8554/live.sdp"
+SERVER = "rtsp://localhost:8554/live.sdp"
 
 (省略)
 
+```
+
+
+* リトライ
+    * リトライとして一般的なExponential Backoff(指数関数的後退)を利用しています。
+    * （リトライ間隔が「倍、倍・・」になるアルゴリズム）
+    * 最大遅延を60secとし、初回の再実行時間遅延を5secにしています。
+
+```python
+@retry(delay=5, backoff=2, max_delay=60)
 ```
--- pystreamer.py
+++ pystreamer.py
@@ -5,7 +5,6 @@ import os
 import argparse
 import traceback
 import logging
-import uuid
 import asyncio
 import time
 
@@ -35,12 +34,64 @@ fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
 logging.basicConfig(level=logging.INFO, filename='app.log', format=fmt)
 
 
-@retry()
-async def capture_stream(server):
+@retry(delay=5, backoff=2, max_delay=60)
+@asyncio.coroutine
+def capture_stream_1(server):
     i = 0
+    logger.info('T1')
     logger.info('request server address is {0}'.format(server))
 
-    loop = asyncio.get_event_loop()
+    # loop = asyncio.get_event_loop()
+
+    cap = cv2.VideoCapture(server)
+
+    is_exist = cap.isOpened()
+
+    # streamがOpenできない場合はreturnする
+    if not is_exist:
+        logger.warning('{0} can not open stream'.format(server))
+        return
+    # streamがOpenできる場合は処理を継続する
+    else:
+        # await loop.run_in_executor(None)
+
+        logger.info('{0} capture stream start'.format(server))
+        # __capture(i, cap, 't1')
+        while (cap.isOpened()):
+            ret, frame = cap.read()
+
+            yield from asyncio.sleep(1)
+
+            logger.info('t1 {0} streaming...'.format(server))
+
+            # debug指定時はGUIに映像ストリームを表示する
+            if args.debug:
+                cv2.imshow('Raw Frame', frame)
+
+                # 映像ストリームの取得が始まった場合、 `q` を入力すると break する
+                if cv2.waitKey(20) & 0xFF == ord('q'):
+                    break
+            # 通常はimwriteによりスライスした静止画を保存する
+            else:
+                file_name = 't1' + '_' + str(i) + '.png'
+                file_name = save_dir + file_name
+
+                time.sleep(1)
+                cv2.imwrite(file_name, frame)
+                i += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+@retry(delay=5, backoff=2, max_delay=60)
+@asyncio.coroutine
+def capture_stream_2(server):
+    i = 0
+    logger.info('T2')
+    logger.info('request server address is {0}'.format(server))
+
+    # loop = asyncio.get_event_loop()
 
     cap = cv2.VideoCapture(server)
 
@@ -56,81 +107,69 @@ async def capture_stream(server):
 
         logger.info('{0} capture stream start'.format(server))
 
-        await loop.run_in_executor(__capture(i, cap))
-        # stream_id = None
-        #
-        # while (cap.isOpened()):
-        #     ret, frame = cap.read()
-        #
-        #     # debug指定時はGUIに映像ストリームを表示する
-        #     if args.debug:
-        #         cv2.imshow('Raw Frame', frame)
-        #
-        #         # 映像ストリームの取得が始まった場合、 `q` を入力すると break する
-        #         if cv2.waitKey(20) & 0xFF == ord('q'):
-        #             break
-        #     # 通常はimwriteによりスライスした静止画を保存する
-        #     else:
-        #
-        #         if stream_id is None:
-        #             stream_id = str(uuid.uuid4())
-        #         file_name = stream_id + '_' + str(i) + '.png'
-        #         file_name = save_dir + file_name
-        #
-        #         time.sleep(1)
-        #         cv2.imwrite(file_name, frame)
-        #         i += 1
-        #
-        # cap.release()
-        # cv2.destroyAllWindows()
+        # __capture(i, cap, 't2')
+
+        while (cap.isOpened()):
+
+            yield from asyncio.sleep(1)
+
+            ret, frame = cap.read()
+
+            logger.info('t2 {0} streaming...'.format(server))
+
+            # debug指定時はGUIに映像ストリームを表示する
+            if args.debug:
+                cv2.imshow('Raw Frame', frame)
+
+                # 映像ストリームの取得が始まった場合、 `q` を入力すると break する
+                if cv2.waitKey(20) & 0xFF == ord('q'):
+                    break
+            # 通常はimwriteによりスライスした静止画を保存する
+            else:
+                file_name = 't2' + '_' + str(i) + '.png'
+                file_name = save_dir + file_name
+
+                time.sleep(1)
+                cv2.imwrite(file_name, frame)
+                i += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
-def __capture(i, cap):
-    stream_id = None
-
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-
-        # debug指定時はGUIに映像ストリームを表示する
-        if args.debug:
-            cv2.imshow('Raw Frame', frame)
-
-            # 映像ストリームの取得が始まった場合、 `q` を入力すると break する
-            if cv2.waitKey(20) & 0xFF == ord('q'):
-                break
-        # 通常はimwriteによりスライスした静止画を保存する
-        else:
-
-            if stream_id is None:
-                stream_id = str(uuid.uuid4())
-            file_name = stream_id + '_' + str(i) + '.png'
-            file_name = save_dir + file_name
-
-            time.sleep(1)
-            cv2.imwrite(file_name, frame)
-            i += 1
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-def multi_capture_streams(camera_list):
-    loop = asyncio.get_event_loop()
-    print(camera_list[0])
-    print(camera_list[1])
-
-    gather = asyncio.gather(
-        capture_stream(camera_list[0]),
-        capture_stream(camera_list[1])
-    )
-    loop.run_until_complete(gather)
+# def __capture(i, cap, t):
+#
+#     while (cap.isOpened()):
+#         ret, frame = cap.read()
+#
+#         # debug指定時はGUIに映像ストリームを表示する
+#         if args.debug:
+#             cv2.imshow('Raw Frame', frame)
+#
+#             # 映像ストリームの取得が始まった場合、 `q` を入力すると break する
+#             if cv2.waitKey(20) & 0xFF == ord('q'):
+#                 break
+#         # 通常はimwriteによりスライスした静止画を保存する
+#         else:
+#             file_name = t + '_' + str(i) + '.png'
+#             file_name = save_dir + file_name
+#
+#             time.sleep(1)
+#             cv2.imwrite(file_name, frame)
+#             i += 1
+#
+#     cap.release()
+#     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     try:
         if len(args.cameras) != 0:
-            multi_capture_streams(args.cameras)
+            loop = asyncio.get_event_loop()
+            tasks = asyncio.wait([capture_stream_1(args.cameras[0]), capture_stream_2(args.cameras[1])])
+            loop.run_until_complete(tasks)
+
         else:
-            capture_stream(SERVER)
+            capture_stream_1(SERVER)
     except:
         logger.error(traceback.format_exc())
